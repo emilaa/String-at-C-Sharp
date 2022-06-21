6cd5a3ee4697c574b9d7fde1d2d2407f412795ea
emilaa <emilaa@code.edu.az>
2022-06-22 03:04:32 +0400

    New add 'cs.' file

---
+++ String at C Sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String_at_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "E", "m", "i", "l" };
+            Name(names);
+        }
+       
+        //1.Reverse the name.
+        static void Name(string[] names)
+        {
+            int count = 0;
+
+            for (int i = 3; i < names.Length; i--)
+            {
+                count--;
+                
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
